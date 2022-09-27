c1937f24f84deca877007bd6243009790a68624b
Nadja Morin <nadja.morin@desinata.se>
2022-09-27 15:43:05 +0200

    Add project files.

---
+++ TeacherBot_NT/Program.cs
@@ -0,0 +1,12 @@
+﻿using TeacherBot2;
+
+//Get percentage
+var gradeCalculator = new GradeCalculator();
+
+Console.WriteLine("Enter the percentage: ");
+var percentage = Convert.ToInt32(Console.ReadLine());
+
+var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+//Print grade
+Console.WriteLine($"Student Grade: {grade}");
